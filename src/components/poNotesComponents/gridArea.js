b49a3ce2068d46a24f978e686f7747e96f088da3
samim-hossain-mondal <Samim_Hossain_Mondal@mckinsey.com>
2023-01-28 19:49:06 +0530

    feat: add many functionalities

--- src/components/poNotesComponents/gridArea.js
+++ src/components/poNotesComponents/gridArea.js
@@ -1,84 +1,42 @@
 import * as React from 'react';
 import Grid from '@mui/material/Grid';
-import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
 import ActionItemsTable from './POtables/ActionItemsTable';
 import KeyDecisionTable from './POtables/KeyDecisionsTable';
 import AgendaTable from './POtables/AgendaTable';
-
-export default function griArea() {
+export default function GridArea() {
   return (
-   /* <div style={{ width: '100%' }}>
-      <Box
-        sx={{
-          p: 6,
-          m: 1,
-          display: 'flex',
-          flexDirection: 'row',
-          width:'500px'
-         
-        }}
-      >
-        <Box sx={{
-          p: 6,
-          m: 1,
-          display: 'flex',
-          flexDirection: 'row',
-          width:'500px'}}
-         > <ActionItemsTable/></Box>
-        <Box sx={{
-          p: 6,
-          m: 1,
-          display: 'flex',
-          flexDirection: 'row',
-          width:'500px'}}
-         > <KeyDecisionTable/></Box>
-       
-       <Box sx={{
-          p: 6,
-          m: 1,
-          display: 'flex',
-          flexDirection: 'row',
-          width:'500px'}}
-         > <AgendaTable/></Box>
-  
-      </Box>*/
-      <div style={{ width: '100%' }}>
+    <Box style={{ width: '100%' }}>
       <Grid container spacing={3} columns={20}>
-  <Grid item xs={1}>
-    
-  </Grid>
-  <Grid sx={{
+        <Grid item xs={1}>
+        </Grid>
+        <Grid sx={{
           p: 6,
           m: 1,
           display: 'flex',
-          flexDirection: 'row',
-          width:'500px'}}item xs={6}>
-    <ActionItemsTable/>
-  </Grid>
-  
-  <Grid sx={{
+          flexDirection: 'row'
+        }} item xs={6}>
+          <ActionItemsTable />
+        </Grid>
+        <Grid sx={{
           p: 6,
           m: 1,
           display: 'flex',
-          flexDirection: 'row',
-          width:'500px'}} item xs={6}>
-   <KeyDecisionTable/>
-  </Grid>
-  
-  <Grid sx={{
+          flexDirection: 'row'
+        }} item xs={6}>
+          <KeyDecisionTable />
+        </Grid>
+        <Grid sx={{
           p: 6,
           m: 1,
           display: 'flex',
-          flexDirection: 'row',
-          width:'500px'}} item xs={6}>
-   <AgendaTable/>
-  </Grid>
-  <Grid item xs={1}>
-   
-  </Grid>
-</Grid>
-      
-    </div>
+          flexDirection: 'row'
+        }} item xs={6}>
+          <AgendaTable />
+        </Grid>
+        <Grid item xs={1}>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
--- src/components/routes/poNotes.js
+++ src/components/routes/poNotes.js
@@ -1,19 +1,19 @@
+import React from 'react';
+
+import Box from '@mui/material/Box';
+
 import Navbar from '../dumbComponents/navBar'
 import PoNotesBody from '../poNotesComponents/poNotesBody';
 import PoNotesHeader from '../poNotesComponents/poNotesHeader';
-import GridArea from '../poNotesComponents/gridArea';
-import Box from '@mui/material/Box';
-const poNotesContainer = ()=>{
-    return (
-        <div>
-            <div> <Navbar/> </div>
-            
-            <div><PoNotesHeader/></div><br></br><br></br>
-            <Box sx={{ bgcolor: '#E6EEF2', flexDirection:'row' }}>
-          <GridArea/>
-        </Box>
-        </div>
-    );
+
+function poNotesContainer() {
+  return (
+    <Box>
+      <Box> <Navbar /> </Box>
+      <Box> <PoNotesHeader /> </Box> <br /> <br />
+      <Box> <PoNotesBody /> </Box>
+    </Box >
+  );
 };
 
 export default poNotesContainer;
